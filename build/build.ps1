0b2dbea36f3b24a942e040cdf27e0e5ff0a5ee90
Emberson, Mark <mark.emberson@willistowerswatson.com>
2021-10-16 20:42:08 +0100

    Spark updated to 3.1.2, Windows build fixes

--- build/build.ps1
+++ build/build.ps1
@@ -13,11 +13,31 @@ $yml_content = Get-Content build.yml
 $SHOULD_BUILD_BASE = [regex]::Match($yml_content, 'build_base: "(.+?)"').Groups[1].value
 $SHOULD_BUILD_SPARK = [regex]::Match($yml_content, 'build_spark: "(.+?)"').Groups[1].value
 $SHOULD_BUILD_JUPYTERLAB = [regex]::Match($yml_content, 'build_jupyterlab: "(.+?)"').Groups[1].value
+$SHOULD_BUILD_AIRFLOW = [regex]::Match($yml_content, 'build_airflow: "(.+?)"').Groups[1].value
 
-$SCALA_VERSION = [regex]::Match($yml_content, 'scala: "(.+?)"').Groups[1].value
 $SPARK_VERSION = [regex]::Match($yml_content, 'spark: "(.+?)"').Groups[1].value
-$HADOOP_VERSION = [regex]::Match($yml_content, 'hadoop: "(.+?)"').Groups[1].value
 $JUPYTERLAB_VERSION = [regex]::Match($yml_content, 'jupyterlab: "(.+?)"').Groups[1].value
+$AIRFLOW_VERSION = [regex]::Match($yml_content, 'airflow: "(.+?)"').Groups[1].value
+
+$SPARK_VERSION_MAJOR = $SPARK_VERSION.Substring(0,1)
+Write-Host $SPARK_VERSION_MAJOR
+$SPARK_VERSION_MAJOR = 3
+
+if($SPARK_VERSION_MAJOR = 3) {
+  $HADOOP_VERSION = "3.2"
+  #$SCALA_VERSION = "2.13.4"
+  #$SCALA_KERNEL_VERSION = "0.11.2"
+  $SCALA_VERSION = "2.12.10"
+  $SCALA_KERNEL_VERSION = "0.10.9"
+}
+elseif($SPARK_VERSION_MAJOR = 2) {
+  $HADOOP_VERSION = "2.7"
+  $SCALA_VERSION = "2.11.12"
+  $SCALA_KERNEL_VERSION = "0.6.0"
+}
+else {
+  exit 1
+}
 
 # ----------------------------------------------------------------------------------------------------------------------
 # -- Functions----------------------------------------------------------------------------------------------------------
@@ -38,6 +58,10 @@ function cleanContainers() {
   if($SHOULD_BUILD_BASE){
     docker ps -a | Select-String -Pattern 'base' | ConvertFrom-String | %{docker stop $_.P1; docker rm $_.P1}
   }
+
+  if($SHOULD_BUILD_AIRFLOW){
+    docker ps -a | Select-String -Pattern 'airflow' | ConvertFrom-String | %{docker stop $_.P1; docker rm $_.P1}
+  }
 }
 
 function cleanImages() {
@@ -55,6 +79,10 @@ function cleanImages() {
 
   if($SHOULD_BUILD_BASE){
     docker images | Select-String -Pattern 'base' | ConvertFrom-String | %{docker rmi -f $_.P3}
+  }
+
+  if($SHOULD_BUILD_AIRFLOW){
+    docker images | Select-String -Pattern 'airflow' | ConvertFrom-String | %{docker rmi -f $_.P3}
   }
 
 }
@@ -104,8 +132,18 @@ function buildImages() {
         --build-arg scala_version="${SCALA_VERSION}" `
         --build-arg spark_version="${SPARK_VERSION}" `
         --build-arg jupyterlab_version="${JUPYTERLAB_VERSION}" `
+        --build-arg scala_kernel_version="${SCALA_KERNEL_VERSION}" `
         -f docker/jupyterlab/Dockerfile `
         -t jupyterlab:${JUPYTERLAB_VERSION}-spark-${SPARK_VERSION} .
+    }
+
+  if($SHOULD_BUILD_AIRFLOW){
+    docker build `
+        --build-arg build_date="${BUILD_DATE}" `
+        --build-arg spark_version="${SPARK_VERSION}" `
+        --build-arg airflow_image_version="apache/airflow:${AIRFLOW_VERSION}" `
+        -f docker/airflow/Dockerfile `
+        -t spark-airflow:${AIRFLOW_VERSION}-spark-${SPARK_VERSION} .
     }
 
 }
